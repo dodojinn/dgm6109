52ed8ede5e0c645c9d398e36ad30f8d42f157eda
JingJing Fang <fjj625561217@gmail.com>
2025-09-22 23:14:09 -0700

    Add files via upload

---
+++ Lab 1/F_to_C_and_K.js
@@ -0,0 +1,10 @@
+// Fahrenheit to Celsius and Kelvin conversion
+
+let Fahrenheit = 84;//Wuhan, August 2025 average temperature(≈28.9°C)//
+
+let Celsius = (Fahrenheit - 32) * 5 / 9;
+let Kelvin = Celsius + 273.15
+
+console.log("Temperature (fahrenheit): "+ Fahrenheit);
+console.log("Temperature (celsius):"+ Celsius);
+console.log("Temperature (kelvin):"+Kelvin);
